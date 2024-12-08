3ee71f8fefdd034876ec32cccfc8e55b52770911
Lucas <lalmeida464@gmail.com>
2024-12-08 20:16:48 -0300

    UserDetailsService

---
+++ src/main/java/com/lucasalmeida/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.lucasalmeida.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
